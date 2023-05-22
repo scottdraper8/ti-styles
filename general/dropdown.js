aa333222ea7865ebf01f0398e7632bdc479df5e3
Scott Draper <admin@scottdraper.io>
2023-05-22 16:52:56 -0600

    Update and rename dropdowns.js to dropdown.js

--- general/dropdown.js
+++ general/dropdown.js
@@ -1,5 +1,5 @@
 // Set arrow depending on element state
-const setArrow = (el, event) => {
+let setArrow = (el, event) => {
     if (event.target.hasAttribute("open")) {
         el.innerText = el.innerText.replace(/►/, '▼')
     } else {
@@ -8,8 +8,8 @@ const setArrow = (el, event) => {
 }
 
 // Event listener for element toggle
-const detailsEls = document.querySelectorAll('details')
-const summaryEls = document.querySelectorAll('details summary')
+let detailsEls = document.querySelectorAll('details')
+let summaryEls = document.querySelectorAll('details summary')
 for (let i = 0; i < detailsEls.length; i++) {
     summaryEls[i].innerText = '►' + summaryEls[i].innerText
     detailsEls[i].addEventListener('toggle', (e) => setArrow(summaryEls[i], e))
