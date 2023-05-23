a2b8cd1f249ab945cacd39075cd3f72a56ccd46c
Scott Draper <admin@scottdraper.io>
2023-05-22 20:55:00 -0600

    Added background and scrollbar logic

--- addStyles.js
+++ addStyles.js
@@ -1,5 +1,3 @@
-/* ADD ARROW FUNCTIONALITY TO <details>
----------------------------------------------------------- */
 // Set arrow depending on element state
 let setArrow = (el, event) => {
     if (event.target.hasAttribute("open")) {
@@ -20,7 +18,7 @@ for (let i = 0; i < detailsEls.length; i++) {
 
 /* ADD EXTERNAL STYLESHEETS
 ---------------------------------------------------------- */
-function addStylesheet(url) {
+let addStylesheet = (url) => {
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = url;
@@ -32,3 +30,38 @@ addStylesheet("https://cdn.jsdelivr.net/gh/scottdraper8/ti-styles/dillinger.css"
 
 /* EDIT SCROLLBARS & BACKGROUND IMAGE
 ---------------------------------------------------------- */
+// Define page vars
+let scrollbarColor = 'white'
+let path = window.location.pathname.split('/')[3]
+
+// Change the background and scrollbar color for each course
+switch (path) {
+    case 'daily-coding-challenges-200340-22-may-2023-16-august-2023':
+        scrollbarColor = 'var(--link-color)'
+        document.body.style.backgroundImage = 'url("https://i.imgur.com/RjgrVb4.jpg")'
+        break
+    case 'html-and-css-200340-22-may-2023-16-august-2023':
+        scrollbarColor = '#71b5bd'
+        document.body.style.backgroundImage = 'url("https://i.imgur.com/88zrin1.jpg")'
+        break
+    case 'front-end-fundamentals-200340-22-may-2023-16-august-2023':
+        document.body.style.backgroundImage = 'url("https://i.imgur.com/tMd0wGQ.jpg")'
+        break
+    case 'full-stack-development-200340-22-may-2023-16-august-2023':
+        scrollbarColor = 'var(--link-color)'
+        document.body.style.backgroundImage = 'url("https://i.imgur.com/AgHy3aa.jpg")'
+        break
+    case 'python-and-django-200340-22-may-2023-16-august-2023':
+        scrollbarColor = '#67778E'
+        document.body.style.backgroundImage = 'url("https://i.imgur.com/39ceaFj.jpg")'
+        break
+    case 'react-fundamentals-200340-22-may-2023-16-august-2023':
+        document.body.style.backgroundImage = 'url("https://i.imgur.com/ceYKe1O.jpg")'
+        break
+}
+
+// Set the scrollbar color
+let scrollThumbs = document.querySelectorAll('::-webkit-scrollbar-thumb')
+for (let thumb of scrollThumbs) {
+    thumb.style.background = scrollbarColor
+}
